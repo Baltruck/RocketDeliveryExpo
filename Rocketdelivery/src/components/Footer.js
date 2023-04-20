68c796ca73bb76304c0953191a97c60a9a3842ed
Baltruck <optimix@live.ca>
2023-04-20 10:33:35 -0400

    footer ok

--- Rocketdelivery/src/components/Footer.js
+++ Rocketdelivery/src/components/Footer.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import globalStyles from '../globalStyles';
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 
 const Footer = () => {
   const navigation = useNavigation();
   return (
     <View style={styles.footerContainer}>
-      <Text style={globalStyles.title}>Mon footer2</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Restaurants')}>
+      <TouchableOpacity
+        style={styles.footerElement}
+        onPress={() => navigation.navigate('Restaurants')}
+      >
+        {/* <FontAwesomeIcon icon="fa-solid fa-burger" /> */}
+        <FontAwesome5 name="hamburger" size={24} color="black" />
         <Text style={styles.footerText}>Restaurants</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.footerElement}
+        onPress={() => navigation.navigate('OrderHistory')}
+      >
+        <FontAwesome5 name="history" size={24} color="black" />
+        <Text style={styles.footerText}>OrderHistory</Text>
       </TouchableOpacity>
     </View>
   );
@@ -18,19 +31,21 @@ const Footer = () => {
 
 const styles = StyleSheet.create({
   footerContainer: {
-    backgroundColor: '#f4511e',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
     borderTopWidth: 1,
     borderTopColor: '#fff',
   },
+  footerElement: {
+    alignItems: 'center',
+  },
   footerText: {
-    color: '#fff',
+    color: '#000',
     fontSize: 16,
-    marginTop: 10,
+    marginTop: 5,
   },
 });
 
 export default Footer;
-
