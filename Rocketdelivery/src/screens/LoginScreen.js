fdc7e2b7f11aa124b59b5ae4a64db88d7b27de77
Baltruck <optimix@live.ca>
2023-04-21 12:25:16 -0400

    restaurant fetch ok

--- Rocketdelivery/src/screens/LoginScreen.js
+++ Rocketdelivery/src/screens/LoginScreen.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { View, TextInput, StyleSheet, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from 'react-native';
-import { useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import redirApi from '../components/NgrokUrl';
+import React from "react";
+import { View, TextInput, StyleSheet, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "react-native";
+import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import redirApi from "../components/NgrokUrl";
 
 const storeData = async (key, value) => {
   try {
@@ -15,39 +15,38 @@ const storeData = async (key, value) => {
 };
 
 const LoginScreen = () => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const navigation = useNavigation();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleLogin = () => {
-      fetch(`${redirApi}api/login`, {
-      method: 'POST',
+    fetch(`${redirApi}api/login`, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email,
-        password
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // Stock the user_id, customer_id in local storage
+          storeData("user_id", data.user_id.toString());
+          storeData("customer_id", data.customer_id.toString());
+
+          // navigation.navigate('Restaurants');
+          navigation.navigate("Home", { screen: "Restaurants" });
+        } else {
+          setError("Invalid email or password");
+        }
       })
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        // Stock the user_id, customer_id in local storage
-        storeData('user_id', data.user_id.toString());
-        storeData('customer_id', data.customer_id.toString());
-
-        // navigation.navigate('Restaurants');
-        navigation.navigate('Home', { screen: 'Restaurants' });
-
-      } else {
-        setError('Invalid email or password');
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
@@ -72,23 +71,23 @@ const LoginScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 4,
-      padding: 10,
-      marginVertical: 10,
-      width: '80%',
-    },
-    error: {
-      color: 'red',
-      marginVertical: 10
-    }
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 10,
+    marginVertical: 10,
+    width: "80%",
+  },
+  error: {
+    color: "red",
+    marginVertical: 10,
+  },
+});
 
 export default LoginScreen;
--- Rocketdelivery/src/screens/RestaurantsScreen.js
+++ Rocketdelivery/src/screens/RestaurantsScreen.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity, View, Text, StyleSheet, ScrollView, Image, Dimensions } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Dimensions,
+} from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { Card } from "react-native-elements";
 import { FontAwesome as Icon } from "@expo/vector-icons";
 import globalStyles from "../globalStyles";
-import { Asset } from "expo-asset";
-import Constants from "expo-constants";
-import { useNavigation } from '@react-navigation/native';
-import MenuScreen from './MenuScreen';
+import { useNavigation } from "@react-navigation/native";
+import redirApi from "../components/NgrokUrl";
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -16,42 +22,40 @@ const RestaurantsScreen = () => {
   const [selectedRating, setSelectedRating] = useState("all");
   const [selectedPrice, setSelectedPrice] = useState("all");
   const [restaurantImages, setRestaurantImages] = useState([]);
-  
 
-// Create a function to render the price range component
-const renderPriceRange = (priceRange) => {
+  // Function to render the price range component
+  const renderPriceRange = (priceRange) => {
     let priceSymbol = "";
-  
+
     for (let i = 0; i < priceRange; i++) {
       priceSymbol += "$";
     }
-  
+
     return <Text style={styles.priceRangeText}>({priceSymbol})</Text>;
   };
 
+  // send id to menu screen
+  const navigation = useNavigation();
+  const handleCardPress = (restaurantId) => {
+    navigation.navigate("Menu", { restaurantId });
+  };
 
-// send id to menu screen
-const navigation = useNavigation();
-const handleCardPress = (restaurantId) => {
-  navigation.navigate('Menu', { restaurantId });
-};
-
-// import image for random restaurant images
-const localImages = [
-  require("../../assets/Images/Restaurants/cuisineGreek.jpg"),
-  require("../../assets/Images/Restaurants/cuisineJapanese.jpg"),
-  require("../../assets/Images/Restaurants/cuisinePasta.jpg"),
-  require("../../assets/Images/Restaurants/cuisinePizza.jpg"),
-  require("../../assets/Images/Restaurants/cuisineSoutheast.jpg"),
-  require("../../assets/Images/Restaurants/cuisineViet.jpg"),
-];
+  // import image for random restaurant images
+  const localImages = [
+    require("../../assets/Images/Restaurants/cuisineGreek.jpg"),
+    require("../../assets/Images/Restaurants/cuisineJapanese.jpg"),
+    require("../../assets/Images/Restaurants/cuisinePasta.jpg"),
+    require("../../assets/Images/Restaurants/cuisinePizza.jpg"),
+    require("../../assets/Images/Restaurants/cuisineSoutheast.jpg"),
+    require("../../assets/Images/Restaurants/cuisineViet.jpg"),
+  ];
 
   useEffect(() => {
     // Set local restaurant images
     setRestaurantImages(localImages);
 
     // Fetch restaurants data from database
-    fetch("https://2ee4-74-50-186-92.ngrok-free.app/api/restaurants")
+    fetch(`${redirApi}api/restaurants`)
       .then((response) => response.json())
       .then((data) => {
         setRestaurants(data);
@@ -69,9 +73,8 @@ const localImages = [
     setSelectedPrice(value);
   };
 
-
- // Filter restaurants based on selected rating and price
- const filteredRestaurants = restaurants.filter((restaurant) => {
+  // Filter restaurants based on selected rating and price
+  const filteredRestaurants = restaurants.filter((restaurant) => {
     if (selectedRating !== "all") {
       const rating = restaurant.restaurant_rating;
       const lowerBound = parseFloat(selectedRating) - 0.5;
@@ -110,80 +113,91 @@ const localImages = [
     return true;
   });
 
-  
-  
-
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-      <Text style={[styles.headerText, globalStyles.title]}>NEARBY RESTAURANTS</Text>
-      <View style={styles.filterContainer}>
-      <View style={styles.pickerWrapper}>
-          <Text style={styles.filterText}>Rating</Text>
-          <View style={styles.scrollMenuStyle}>
-          <Picker
-            selectedValue={selectedRating}
-            style={{ height: 50, width: 150, color: 'white', marginHorizontal: -4 }}
-            itemStyle={styles.pickerItem}
-            onValueChange={(itemValue) => handleRatingChange(itemValue)}
-          >
-            <Picker.Item label="-- Select --"  value="all" />
-            <Picker.Item label="1" value="1" />
-            <Picker.Item label="2" value="2" />
-            <Picker.Item label="3" value="3" />
-            <Picker.Item label="4" value="4" />
-            <Picker.Item label="5" value="5" />
-          </Picker>
+        <Text style={[styles.headerText, globalStyles.title]}>
+          NEARBY RESTAURANTS
+        </Text>
+        <View style={styles.filterContainer}>
+          <View style={styles.pickerWrapper}>
+            <Text style={styles.filterText}>Rating</Text>
+            <View style={styles.scrollMenuStyle}>
+              <Picker
+                selectedValue={selectedRating}
+                style={{
+                  height: 50,
+                  width: 150,
+                  color: "white",
+                  marginHorizontal: -4,
+                }}
+                itemStyle={styles.pickerItem}
+                onValueChange={(itemValue) => handleRatingChange(itemValue)}
+              >
+                <Picker.Item label="-- Select --" value="all" />
+                <Picker.Item label="1" value="1" />
+                <Picker.Item label="2" value="2" />
+                <Picker.Item label="3" value="3" />
+                <Picker.Item label="4" value="4" />
+                <Picker.Item label="5" value="5" />
+              </Picker>
+            </View>
+          </View>
+          <View style={styles.pickerWrapper}>
+            <Text style={styles.filterText}>Price</Text>
+            <View style={styles.scrollMenuStyle}>
+              <Picker
+                selectedValue={selectedPrice}
+                style={{
+                  height: 50,
+                  width: 150,
+                  color: "white",
+                  marginHorizontal: -4,
+                }}
+                itemStyle={styles.pickerItem}
+                onValueChange={(itemValue) => handlePriceChange(itemValue)}
+              >
+                <Picker.Item label="-- Select --" value="all" />
+                <Picker.Item label="$" value="$" />
+                <Picker.Item label="$$" value="$$" />
+                <Picker.Item label="$$$" value="$$$" />
+                <Picker.Item label="$$$$" value="$$$$" />
+              </Picker>
+            </View>
           </View>
         </View>
-        <View style={styles.pickerWrapper}>
-          <Text style={styles.filterText}>Price</Text>
-          <View style={styles.scrollMenuStyle}>
-          <Picker
-            selectedValue={selectedPrice}
-            style={{ height: 50, width: 150, color: 'white', marginHorizontal: -4 }}
-            itemStyle={styles.pickerItem}
-            onValueChange={(itemValue) => handlePriceChange(itemValue)}
-          >
-            <Picker.Item label="-- Select --" value="all" />
-            <Picker.Item label="$" value="$" />
-            <Picker.Item label="$$" value="$$" />
-            <Picker.Item label="$$$" value="$$$" />
-            <Picker.Item label="$$$$" value="$$$$" />
-          </Picker>
-          </View>
-        </View>
-      </View>
       </View>
       <Text style={styles.cardTitle}>RESTAURANTS</Text>
       <ScrollView contentContainerStyle={styles.cardsContainer}>
-      {filteredRestaurants.map((restaurant, index) => {
-
+        {filteredRestaurants.map((restaurant, index) => {
           return (
-            <TouchableOpacity key={restaurant.id} onPress={() => handleCardPress(restaurant.id)}>
-             {/* <TouchableOpacity onPress={handleCardPress}> */}
-            <Card key={index} containerStyle={styles.card}>
-              <Image
-                source={
-                  restaurantImages[
-                    Math.floor(Math.random() * restaurantImages.length)
-                  ]
-                }
-                style={styles.cardImage}
-              />
-{/* Modify the restaurant name and price range rendering */}
-<View style={styles.restaurantNameContainer}>
+            <TouchableOpacity
+              key={restaurant.id}
+              onPress={() => handleCardPress(restaurant.id)}
+            >
+              <Card key={index} containerStyle={styles.card}>
+                <Image
+                  source={
+                    restaurantImages[
+                      Math.floor(Math.random() * restaurantImages.length)
+                    ]
+                  }
+                  style={styles.cardImage}
+                />
+
+                <View style={styles.restaurantNameContainer}>
                   <Text style={styles.cardTitle}>{restaurant.name}</Text>
                   {renderPriceRange(restaurant.price_range)}
                 </View>
-              <View style={styles.cardStars}>
-              {[...Array(parseInt(restaurant.restaurant_rating))].map((e, i) => (
-                  <Icon key={i} name="star" style={styles.starIcon} />
-                ))}
-              </View>
-            </Card>
-             </TouchableOpacity>
+                <View style={styles.cardStars}>
+                  {[...Array(parseInt(restaurant.restaurant_rating))].map(
+                    (e, i) => (
+                      <Icon key={i} name="star" style={styles.starIcon} />
+                    )
+                  )}
+                </View>
+              </Card>
+            </TouchableOpacity>
           );
         })}
       </ScrollView>
@@ -192,126 +206,119 @@ const localImages = [
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 10,
-    },
-    header: {
-      backgroundColor: "#fff",
-      padding: 20,
-    },
-    headerText: {
-      fontWeight: "bold",
-      fontSize: 20,
-      transform: [
-        { scaleY: 1.2 }, 
-        { scaleX: 0.8 }, 
-      ],
-      marginLeft: -41,
-        marginTop: -15,
-        marginBottom: 20,
-        fontFamily: 'Oswald-Regular',
-    },
-    filterContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-      marginTop: 10,
-    },
-    pickerWrapper: {
-        width: "45%",
-      },
-    pickerContainer: {
-        flexDirection: "column",
-        alignItems: "flex-start",
-      },
-    filterItem: {
-        flexDirection: "column",
-        alignItems: "flex-start",
-      },
-    filterText: {
-      fontSize: 19,
-      fontWeight: "bold",
-      marginBottom: 5,
-      fontFamily: 'Oswald-Regular',
-      transform: [
-        { scaleY: 1.3 }, 
-        { scaleX: 0.8 }, 
-      ],
-      marginLeft: -17,
-    },
-    filterPicker: {
-      flex: 1,
-      height: 50,
-      backgroundColor: "#DA583B",
-      borderRadius: 20,
-      color: "#fff",
-    },
-    pickerItem: {
-        textAlign: 'center', 
-        justifyContent: 'center', 
-      },
-    card: {
-      width: screenWidth * 0.39,
-      marginBottom: 10,
-      borderRadius: 7,
-      overflow: "hidden",
-      paddingHorizontal: 0,
-      paddingVertical: 0,
-      paddingBottom: 10,
-    },
-    cardImage: {
-      borderWidth: 0,
-      width: "100%",
-      height: 110,
-      resizeMode: "cover",
-    },
-    cardTitle: {
-      fontSize: 14,
-      fontWeight: "bold",
-        marginTop: 10,
-        marginBottom: 10,
-    },
-    cardStars: {
-      flexDirection: "row",
-      alignItems: "center",
-      marginLeft: 10,
-      marginBottom: 10,
-    },
-    starIcon: {
-      color: "#000000",
-      marginRight: 2,
-      fontSize: 10,
-    },
-    cardsContainer: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-evenly",
-        padding: 0,
-      },
-      scrollMenuStyle: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#DA583B",
-        borderRadius: 7,
-        borderWidth: 0,
-        height: 40,
-        alignItems: "center",
-    },
-    restaurantNameContainer: {
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        paddingHorizontal: 10,
-      },
-      
-      priceRangeText: {
-        fontSize: 14,
-        fontWeight: "bold",
-        marginLeft: 4,
-      },
-  });
-  
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  header: {
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    transform: [{ scaleY: 1.2 }, { scaleX: 0.8 }],
+    marginLeft: -41,
+    marginTop: -15,
+    marginBottom: 20,
+    fontFamily: "Oswald-Regular",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  pickerWrapper: {
+    width: "45%",
+  },
+  pickerContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  filterItem: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  filterText: {
+    fontSize: 19,
+    fontWeight: "bold",
+    marginBottom: 5,
+    fontFamily: "Oswald-Regular",
+    transform: [{ scaleY: 1.3 }, { scaleX: 0.8 }],
+    marginLeft: -17,
+  },
+  filterPicker: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#DA583B",
+    borderRadius: 20,
+    color: "#fff",
+  },
+  pickerItem: {
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  card: {
+    width: screenWidth * 0.39,
+    marginBottom: 10,
+    borderRadius: 7,
+    overflow: "hidden",
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+    paddingBottom: 10,
+  },
+  cardImage: {
+    borderWidth: 0,
+    width: "100%",
+    height: 110,
+    resizeMode: "cover",
+  },
+  cardTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  cardStars: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  starIcon: {
+    color: "#000000",
+    marginRight: 2,
+    fontSize: 10,
+  },
+  cardsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    padding: 0,
+  },
+  scrollMenuStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DA583B",
+    borderRadius: 7,
+    borderWidth: 0,
+    height: 40,
+    alignItems: "center",
+  },
+  restaurantNameContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+
+  priceRangeText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginLeft: 4,
+  },
+});
 
 export default RestaurantsScreen;
